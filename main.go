397f3686b370e77b39d7d78b9f3f7d8c7acf691f
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2018-06-27 19:33:56 +0200

    Add comments

--- main.go
+++ main.go
@@ -18,12 +18,15 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 	return w.Writer.Write(b)
 }
 
+// Gzip-compress the request like the real API does
 func makeGzipHandler(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			fn(w, r)
 			return
 		}
+
+		// Copy-paste of the headers the real API uses
 		w.Header().Set("Content-Encoding", "gzip")
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("X-IPLB-Instance", "17342")
@@ -33,6 +36,7 @@ func makeGzipHandler(fn http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Accept-Language", "fr,fr-FR")
 		w.Header().Set("Connection", "keep-alive")
 		w.Header().Set("Host", "netfu.net")
+
 		gz := gzip.NewWriter(w)
 		defer gz.Close()
 		gzr := gzipResponseWriter{Writer: gz, ResponseWriter: w}
@@ -41,6 +45,8 @@ func makeGzipHandler(fn http.HandlerFunc) http.HandlerFunc {
 }
 
 func validLicense(w http.ResponseWriter, r *http.Request) {
+	// 1 = license accepted
+	// -1 = rejected
 	fmt.Fprintf(w, "1")
 	fmt.Printf("Received request from %s\n", r.RemoteAddr)
 }
@@ -48,7 +54,7 @@ func validLicense(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", makeGzipHandler(validLicense))
 	http.ListenAndServe(":80", nil)
-	fmt.Println("server running at http://localhost:80")
+	fmt.Println("Server running at http://localhost:80")
 
 	for {
 		time.Sleep(10 * time.Second)
